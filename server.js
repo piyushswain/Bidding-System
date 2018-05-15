b664cfe0f9393ddb19131a64167491cf92d604ea
ps9793 <piyush.swain3@gmail.com>
2018-05-16 00:25:59 +0530

    Timer and Winner Acknowledgement Added

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
+//Require Variables  
   var express = require('express');
   var app = express();
   var server = require('http').createServer(app);
   var io = require('socket.io')(server);
-  var bid_price = 1000;
+  var events = require('events');
+  var emitter = new events.EventEmitter();
+  
+//Functional Variables
+  var bid_price = 500;
   var next_bid = bid_price+(bid_price*0.1);
-  var start_timer = 0;
+  var bid_start = false;
+  var time = 60;
+  var timer;
+  var highest_bidder;
+
+  function startBid(){
+  timer = setInterval(function(){
+  			  showTime()} , 1000);
+  }
+
+  function showTime(){
+  	time -= 1;
+  	console.log(time);
+  	if(time == 0)
+  		emitter.emit('end');
+  }
+
   app.get('/', function(req, res, next) {
   	res.sendFile(__dirname + '/public/index.html')
   });
@@ -12,20 +33,30 @@
   app.use(express.static('public'));
 
   io.on('connection', function(client) {
-  	console.log('Client connected...');
+  	console.log('Client connected...'+client.id);
+  	client.emit('assign',client.id);
 
   	client.on('join', function(data) {
   		console.log(data);
-  		client.emit('initiate', { old:bid_price, new:next_bid })
+  		client.emit('initiate', { old:bid_price, new:next_bid})
   	});
 
   	client.on('bid', function(data){
+  		if(bid_start == false)
+  			startBid();
+  		bid_start = true;
+
   		console.log(data);
-  		bid_price = Number(data);
+  		highest_bidder = data.c_id;
+  		bid_price = Number(data.bid);
   		next_bid = Math.round(bid_price+(bid_price*0.1));
   		client.emit('update', { old:bid_price, new:next_bid });
   		client.broadcast.emit('update', { old:bid_price, new:next_bid });
   	});
+
+  	emitter.on('end',function(){
+  		client.emit('end',highest_bidder);
+  		clearInterval(timer)});
   });
 
   server.listen(3000);
